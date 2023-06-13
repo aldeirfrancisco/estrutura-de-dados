d587978a8888bda12a76fcab00a1b6fd2200b9f2
aldeir <dide123francisco@gmail.com>
2023-06-13 16:03:15 -0300

    estrutura de dados lista encadeadas.

---
+++ src/estrutura_aula3_lista_ligada/Programa.java
@@ -0,0 +1,15 @@
+package estrutura_aula3_lista_ligada;
+
+public class Programa {
+	public static void main(String[] args) {
+		 ListaLigada lista = new ListaLigada();
+		    lista.adicionar("AC");
+		    lista.adicionar("BA");
+		    lista.adicionar("CE");
+		    lista.adicionar("DF");
+		    System.out.println("Tamanho "+lista.getTamanho() );
+		    System.out.println("Primeiro "+lista.getPrimeiro().getValor() );
+		    System.out.println("Último "+lista.getUltimo().getValor() );
+	}
+  
+}
