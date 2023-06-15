ea3f22abffac7e51b5ba7a0f3627c202cc80e24a
aldeir <dide123francisco@gmail.com>
2023-06-15 19:37:51 -0300

    refatorando mais ainda os métodos de ordenação

--- src/algoritmoLivro/OrdenacaoMetodo.java
+++ src/algoritmoLivro/OrdenacaoMetodo.java
@@ -3,36 +3,28 @@ package algoritmoLivro;
 public class OrdenacaoMetodo {
    
 	public static int[] ordenacaoPorSelecao(int[] array) {
-		int[] novoArray = new int[array.length];
-	
-		for(int i= 0; i < array.length; i++) {
-			int index = buscarIndexMenorNumero(array,i);
-			trocarElementos(array, i, index);
-			
-		}
-		for (int i = 0; i < array.length - 1; i++) {
-            int indiceMenor = i;
-
-            indiceMenor = buscarIndexMenorNumero(array,i);
- 
-            if (indiceMenor != i) {
-                trocarElementos(array, i, indiceMenor);
-            }
-        }
+		 for (int i = 0; i < array.length - 1; i++) {
+	            int indiceMenor = i;
+	            
+	            for (int j = i + 1; j < array.length; j++) {
+	                if (array[j] < array[indiceMenor]) {
+	                    indiceMenor = j;
+	                   
+	                }
+	            }
+	            
+	            if (indiceMenor != i) {
+	                trocarElementos(array, i, indiceMenor);
+	            }
+	        }
 		return array;
 	}
-	private static int buscarIndexMenorNumero(int[] array, int index) {
-		  int menor = array[index];
-          int menorIndex = index;
-          for(int i = index; i < array.length; i++ ) {
-        	  if(array[i] < menor ) {
-        		  menor = array[i];
-        		  menorIndex = i;
-        		  break;
-        	  }
-          }
-		return menorIndex;
-	}
+	/*
+	 * private static int buscarIndexMenorNumero(int[] array, int index) { int menor
+	 * = array[index]; int menorIndex = index; for(int i = index; i < array.length;
+	 * i++ ) { if(array[i] < menor ) { menor = array[i]; menorIndex = i; break; } }
+	 * return menorIndex; }
+	 */
 	
 	 private static void trocarElementos(int[] array, int indiceA, int indiceB) {
 	        int temp = array[indiceA];
