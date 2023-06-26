4b801716ba7491d6c1c46d93d5b1838345384fb9
aldeir <dide123francisco@gmail.com>
2023-06-26 19:50:37 -0300

    ordenação quick sort

---
+++ src/estrutura_aula3_lista_ligada/SelectionQuickSort.java
@@ -0,0 +1,53 @@
+package estrutura_aula3_lista_ligada;
+
+import java.util.Scanner;
+//aula 13;
+public class SelectionQuickSort {
+  public static void main(String[] args) {
+	
+	  int[] vetor = new int[10];
+      
+      for(int i=0; i < vetor.length; i++){
+          vetor[i] = (int) (Math.random() * vetor.length);
+      }
+      
+      System.out.println("<------------------->");
+      for(int i=0; i < vetor.length; i++){
+          System.out.println(vetor[i]);
+      }
+      
+      quicksort(vetor, 0, vetor.length - 1);
+      
+      System.out.println("\n\nOrdenado");
+      for(int i = 0; i < vetor.length; i++){
+          System.out.print(vetor[i] + " ");
+      }
+  }
+  private  static void quicksort(int[] vetor, int esquerda, int direita){
+      if (esquerda < direita){
+          int p = particao(vetor, esquerda, direita);
+          quicksort(vetor, esquerda, p);
+          quicksort(vetor, p + 1, direita);
+      }
+  }
+  private  static int particao(int[] vetor, int esquerda, int direita){
+      int meio = (int) (esquerda + direita) / 2;
+      int pivot = vetor[meio];
+      int i = esquerda - 1;
+      int j = direita + 1;
+      while(true){
+          do{
+              i++;
+          }while(vetor[i] < pivot);
+          do{
+              j--;
+          }while(vetor[j] > pivot);
+          if (i >= j){
+              return j;
+          }
+          int aux = vetor[i];
+          vetor[i] = vetor[j];
+          vetor[j] = aux;
+      }
+  }
+}
